bc268784c9b43f2fa9f61e0ac3e0ea5b4c0c5a7c
dmolokanov <dmolokanov@sinergo.ru>
2022-07-22 16:39:51 +0300

    .

--- app.js
+++ app.js
@@ -15,4 +15,15 @@ function getFruit(arr) {
     return fruits;
 }
 
+function getFruitInReplace(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] % 3 === 0) {
+            arr[i] = 'Apple'
+        }else if(arr[i] % 5 === 0) {
+            arr[i] = 'Orange'
+        }
+    }
+    return arr;
+}
 console.log(getFruit(arr));
+console.log(getFruitInReplace(arr));
