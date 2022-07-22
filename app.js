98a74799fe6f466149ab94b6a2460aafab44572d
dmolokanov <dmolokanov@sinergo.ru>
2022-07-22 19:40:18 +0300

    .

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const arr = [25, 53, 48, 56, 1929]
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30]
 const fruits = [];
 
-
-function getFruit(arr) {
+function getFruit(fruits,arr) {
+    fruits = fruits.splice(0, fruits.length)
     arr.forEach(elem => {
-        if(elem % 3 === 0){
+        if(elem % 3 === 0 && elem % 5 === 0){
+            fruits.push('Apple Orange')
+        }
+        else if(elem % 3 === 0){
             fruits.push('Apple')
         }else if(elem % 5 === 0){
             fruits.push('Orange')
@@ -17,7 +20,9 @@ function getFruit(arr) {
 
 function getFruitInReplace(arr) {
     for (let i = 0; i < arr.length; i++) {
-        if(arr[i] % 3 === 0) {
+        if(arr[i] % 3 === 0 && arr[i] % 5 === 0) {
+            arr[i] = 'Apple Orange'
+        }else if(arr[i] % 3 === 0) {
             arr[i] = 'Apple'
         }else if(arr[i] % 5 === 0) {
             arr[i] = 'Orange'
@@ -25,5 +30,5 @@ function getFruitInReplace(arr) {
     }
     return arr;
 }
-console.log(getFruit(arr));
+console.log(getFruit(fruits, arr));
 console.log(getFruitInReplace(arr));
