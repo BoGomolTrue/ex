ba04bf8703cd1199b1bde3fcc7fd1a8ad4230efd
dmolokanov <dmolokanov@sinergo.ru>
2022-07-23 19:19:33 +0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30]
-const fruits = [];
 
-function getFruit(fruits,arr) {
+/*function getFruit(fruits,arr) {
     if(fruits.length) {
         fruits = fruits.splice(0, fruits.length)
     }
@@ -18,6 +17,22 @@ function getFruit(fruits,arr) {
         }
     })
     return fruits;
+}*/
+function getFruit(arr) {
+    let fruits = [];
+    fruits = arr.map((elem, index, array) => {
+        if (elem % 3 === 0 && elem % 5 === 0) {
+           return array[index] = 'Apple Orange'
+        } else if (elem % 3 === 0) {
+            return array[index] =  'Apple'
+        } else if (elem % 5 === 0) {
+            return array[index] =  'Orange'
+        } else {
+            return array[index] = elem
+        }
+    })
+    return fruits
+
 }
 
 function getFruitInReplace(arr) {
@@ -32,13 +47,6 @@ function getFruitInReplace(arr) {
     }
     return arr;
 }
-getFruit(fruits, arr)
-getFruit(fruits, arr)
-getFruit(fruits, arr)
-getFruit(fruits, arr)
-getFruit(fruits, arr)
-getFruit(fruits, arr)
-getFruit(fruits, arr)
-getFruit(fruits, arr)
-console.log('t', getFruit(fruits, arr));
-//console.log('t2',getFruitInReplace(arr));
+
+console.log(getFruit(arr));
+console.log(getFruitInReplace(arr));
